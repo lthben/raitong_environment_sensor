6630c9f0e17fc2350c8f7990c2b58a17b4feaa33
Benjamin Low <lthben@gmail.com>
2018-07-24 00:22:40 +0800

    cleaned up code

--- raitong_environment_sensor.ino
+++ raitong_environment_sensor.ino
@@ -10,12 +10,14 @@
         D1 (SCL) - OLED D0 & BH1750 SCL
         D2 (SDA) - OLED D1 & BH1750 SDA
         A0 - moisture sensor A
+        D5 - DHT22
+        D4 - button
 
-    Power for sensors:
-        BH1750 - 3.3V
+    Power for components:
+        BH1750 - 5V
         OLED - 3.3V
-        Moisture - 3.3V
-        DHT22 - 3.3V
+        Moisture - 5V
+        DHT22 - 5V
 
     Notes:
     OLED: Need to remove R3 resistor for oled and short R1, R4, R5, R6
@@ -39,7 +41,7 @@ byte buff[2];
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 DHT dht(DHTPIN, DHTTYPE);
 
-#define OLED_RESET 0
+#define OLED_RESET 0 //not connected actually
 Adafruit_SSD1306 display(OLED_RESET);
 
 #if (SSD1306_LCDHEIGHT != 64)
@@ -50,7 +52,7 @@ Button myButton = Button(D4, PULLUP);
 
 void setup()   {
   Wire.begin();
-  Serial.begin(57600);
+  Serial.begin(9600);
   dht.begin();
   delay(1000); // delay 1 sec
 
@@ -60,32 +62,34 @@ void setup()   {
   display.setCursor(32, 32);
   display.println("Raitong\nEnvironment\nSensor");
   display.display();
+
+  Serial.println("Raitong Environment Sensor");
   delay(2000);
 }
 
 
 void loop() {
 
+  //get all the sensor values
   float temp = dht.readTemperature();
   float h = dht.readHumidity();
 
   int moistValue = analogRead(moistureSensorPin);
   moistValue = map(moistValue, 375, 740, 100, 0);
-  // print out the value you read:
   // float voltage = sensorValue * (1024 / 100);
 
   uint16_t lightVal = 0;
   BH1750_Init(BH1750address);
   delay(200);
-
+  
   if (2 == BH1750_Read(BH1750address))
   {
     lightVal = ((buff[0] << 8) | buff[1]) / 1.2;
   }
   delay(150);
 
-  if (myButton.uniquePress())
-  {
+  if (myButton.uniquePress()) {
+  //display all the sensor values
     Serial.println("Button pressed");
 
     display.setTextSize(1);
@@ -95,22 +99,42 @@ void loop() {
     display.println("C");
     display.println("");
 
+    Serial.print("Temp: ");
+    Serial.print(temp);
+    Serial.println("C");
+    Serial.println("");
+
     display.setTextSize(1);
     display.print("Humidity: ");
     display.print(h);
     display.println("%");
     display.println("");
 
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.println("%");
+    Serial.println("");
+
     display.print("Moist: ");
     display.print(moistValue);
     display.println("%");
     display.println("");
+
+    Serial.print("Moist: ");
+    Serial.print(moistValue);
+    Serial.println("%");
+    Serial.println("");
 
     display.setTextSize(1);
     display.print("Light");
     display.print(" ");
     display.print(lightVal, DEC);
     display.println("[lx]");
+
+    Serial.print("Light");
+    Serial.print(" ");
+    Serial.print(lightVal, DEC);
+    Serial.println("[lx]");
 
     display.display();
     delay(2000);
